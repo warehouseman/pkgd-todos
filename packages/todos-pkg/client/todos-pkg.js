ae77b353a7f8a60a7c35111532a44dfb433462bd
Martin Bramwell <martinhbramwell@gmail.com>
2015-04-30 07:59:12 -0400

    Step 4 :  Forms and Events  Changes to be committed: 	modified:   packages/todos-pkg/client/todos-pkg.html 	modified:   packages/todos-pkg/client/todos-pkg.js

--- packages/todos-pkg/client/todos-pkg.js
+++ packages/todos-pkg/client/todos-pkg.js
@@ -1,6 +1,24 @@
 Template.todos.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
 });  
 
+Template.todos.events({
+  "submit .new-task": function (event) {
+    // This function is called when the new task form is submitted
+
+    var text = event.target.text.value;
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    event.target.text.value = "";
+
+    // Prevent default form submit
+    return false;
+  }
+});
